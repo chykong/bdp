97f6fd85a33a83d37225f6aa28e75cf12d0f8bf2
chykong <chykong@163.com>
2017-07-03 23:43:59 +0800

    添加查询VO类

---
+++ bdp_02/src/main/java/com/critc/plat/util/global/GlobalConst.java
@@ -0,0 +1,15 @@
+package com.critc.plat.util.global;
+
+/**
+ * 全局参数
+ *
+ * @author 孔垂云
+ * @date 2017-05-23
+ */
+public class GlobalConst {
+    /**
+     * 定义全局分页每页数据记录数
+     */
+    public static final int PAGESIZE = 20;
+
+}
